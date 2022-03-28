fc46d7879651e4f684d2dfab3a75afcec0838f16
Andres Bastidas <andresbastidas@quiena.com>
2022-03-28 01:41:06 -0300

    working sphere

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,9 @@
-import React, { Component, useRef, useState  } from 'react';
-import { Canvas, useFrame } from '@react-three/fiber';
-import  { useKTX2 } from  '@react-three/drei'
+import React, { Component, useRef, useState, forwardRef, Suspense, useEffect  } from 'react';
+import { Canvas, useFrame, useThree } from '@react-three/fiber';
+import  { useTexture } from  '@react-three/drei';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
+import '/public/styles.css';
+
 var classes = {
     canvas : {
         background: '#505767',
@@ -11,39 +14,51 @@ var classes = {
     }
 }
 
-function Box(props) {
-    //const texture = useKTX2('./oneworld.jpg')
+const Box = forwardRef((props) => {
+
+
+    const texture = useTexture('/textures/oneworld.jpg')
     // This reference gives us direct access to the THREE.Mesh object
     const ref = useRef()
     // Hold state for hovered and clicked events
     const [hovered, hover] = useState(false)
     const [clicked, click] = useState(false)
     // Subscribe this component to the render-loop, rotate the mesh every frame
-    useFrame((state, delta) => (ref.current.rotation.x += 0.01))
+
+
+    useFrame((state, delta) => (ref.current.rotation.y -= 0.003))
     // Return the view, these are regular Threejs elements expressed in JSX
+
+    useEffect(() => {
+        ref.current.rotation.y = -350
+    }, []);
+
     return (
         <mesh
             {...props}
             ref={ref}
             style={classes.sphere}
-            scale={clicked ? 1.5 : 1}
+            scale={clicked ? 3.5 : 2}
             onClick={(event) => click(!clicked)}
             onPointerOver={(event) => hover(true)}
             onPointerOut={(event) => hover(false)}>
             <sphereGeometry attach="geometry" args={[1, 16, 200]} />
-
-            <meshStandardMaterial color={hovered ? 'hotpink' : 'orange'} />
+            <meshStandardMaterial map={texture}/>
         </mesh>
     )
-}
+});
 
 
 function App(props){
+    //const { camera, gl: { domElement } } = useThree();
     return (
         <Canvas style={classes.canvas}>
             <ambientLight/>
             <pointLight position={[10, 10, 10]}/>
-            <Box position={[0, 0, 0]}/>
+            <Suspense fallback={<></>}>
+                <Box position={[0, 0, 0]}/>
+            </Suspense>
+            {false && <orbitControls args={[camera, domElement]} />}
         </Canvas>
     );
 
